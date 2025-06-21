678194a82b6914723636b6bf1cdd9be3b8cb1d56
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-06-21 15:30:38 +0500

    imtixon5

---
+++ imtixon5.py
@@ -0,0 +1,25 @@
+# 5-oy imtihoni
+
+# 					a varinat
+
+# 1.Fibonacci generatori
+# Generator yozing: kiritilgan `n` ta fibonacci 
+# sonlarini chiqaradi
+
+# 2.n ta butun sonlardan iborat a massiv berilgan. b massivni 
+# hosil qiluvchi decorator yozilsin. b massivning har bir elementi 
+# quyidagicha hisoblanadi:
+# b[k] = a[0] + a[1] + ... + a[k]
+
+# 3.Mashina royxati
+# Car(brand, model, year, mileage) nomli namedtuple.
+# Foydalanuvchi 5 ta mashina kiritadi. Eng 
+# kam yurgan mashinani toping.
+
+# 4.10 ta random son chiqaruvchi threadlar
+# Har bir thread bitta random son chiqarsin. 
+# Ular tartibsiz chiqishi kerak.
+
+# 5.Githubda '5-oy imtihoni varinat' nomli repo ochilsin 
+# va har bir masala alohida commit orqali yuborilsin commit 
+# '1-masala' korinishida bolsin
